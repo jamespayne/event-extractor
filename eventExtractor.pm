ee1379a3a6b0db918036fae0dbf4c02cebaf7380
James Payne <s3435992@student.rmit.edu.au>
2016-03-16 21:27:02 +1100

    Initial commit with clean perl module and sandbox for testing.

---
+++ eventExtractor.pm
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+
+# Scripting Langugae Programming Assignment 1
+# Perl Email Event Extraction Script by James Payne(s3435992)
+# RMIT University 2016
---
+++ sandbox/openfilewithargs.pl
@@ -0,0 +1,6 @@
+
+open (my $fh1, @ARGV[0]) || die "Cannot open the file!\n";
+while(<$fh1>){
+  print $_;
+}
+close($fh);
