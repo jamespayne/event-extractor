5525ac80bd2d70863b8281c3fbfb0ec2a87b3717
James Payne <s3435992@student.rmit.edu.au>
2016-03-16 22:16:12 +1100

    Got basic file and command line argument validation working.

--- eventExtractor.pm
+++ eventExtractor.pm
@@ -3,3 +3,26 @@
 # Scripting Langugae Programming Assignment 1
 # Perl Email Event Extraction Script by James Payne(s3435992)
 # RMIT University 2016
+
+use strict;
+use warnings;
+
+# Start main command line checks and file validation.
+
+# Check to see if the required input file has been supplied and there are no
+# more than 1 arguments for input.
+
+if (!$ARGV[0] || @ARGV > 1) {
+  print "\nYou didn't supply an input file or you have supplied too many\n";
+  print "arguments. Please read the documentation!\n\n";
+  exit;
+}
+
+# Check the file name argument has a .json extension.
+
+if ($ARGV[0] =~ /\.json$/) {
+  print "Valid .json file submitted.\n";
+} else {
+  print "That doesn't appear to be a valid .json file.\n";
+  exit;
+}
