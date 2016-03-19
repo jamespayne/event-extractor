339ecb1a4c6b1feae158db9f348409d71385f2af
James Payne <s3435992@student.rmit.edu.au>
2016-03-19 19:48:47 +1100

    Created dates script for experiments with dates.

---
+++ sandbox/dates.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+# Scripting Langugae Programming Assignment 1
+# Perl Email Event Extraction Script by James Payne(s3435992)
+# RMIT University 2016
+
+use strict;
+use warnings;
