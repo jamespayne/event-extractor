19818dd417370d4e36be03be8b3febee427ab03c
James Payne <s3435992@student.rmit.edu.au>
2016-04-15 08:23:46 +1000

    Added some more regex tests and ignored the output file.

--- sandbox/regex.pl
+++ sandbox/regex.pl
@@ -1,30 +1,15 @@
 #!/usr/bin/env perl
-# local $/=undef;
-#
-# $string = " cat";
-#
-# $string =~ s/\n\r\t\s//g;
-#
-# print "$string";
 
-# Date Cleaner
+use Data::Dumper;
 
-$fwdTextDateMatch = '';
+my $datetime = "We'repleasedtobeworkingwithRMITLINK'sOrientationteamandRUSUtowelcomeallnewstudentstocampusthroughaseriesofexcitingOrientationevents.Thisemailservesasanotificationtomakesureyouknowwhenthemajoreventsareoccurring,toensuretheydon'tinterruptyourworkandsothatyouareabletoencourageallnewstudentstoattend.BrunswickAllStudentsWelcome,23February12:30-1:30pmBrunswickCourtyard.BundooraAllStudentsWelcome,24February12-2pmBundooraWestconcourse.CityAllStudentsWelcome,25February11am-2:30pmAlumniCourtyard,UniversityWay.RUSUWelcomeBash,25February4pm-9pmAlumniCourtyard.CityClubsDay,3March11am-2pmAlumniCourtyard,UniversityWay.Earlynextweekisgoodforus.HowaboutMonday11am?Earlynextweekisgoodforus.HowaboutTuesday11:30AM?Earlynextweekisgoodforus.HowaboutWed.11:30AM?Papersubmissiondeadline:January31,2016.Notificationofacceptance:March15th,2016.Finalpapersubmissionandearlyregistrationdeadli
+ne:Apr.15,2016Papersubmissiondeadline:31January,2016.Notificationofacceptance:15thMarch,2016.Finalpapersubmissionandearlyregistrationdeadline:15t
+hApr.,2016Yes,tomorrow6:00pmisfineforme.Yes,tomorrow6:00pmisfineforme.ThepartywillbeholdonTomorrow9:00pm-2:00am";
 
-my $text = "We're pleased to be working with RMIT LINK's Orientation team and RUSU to welcome all new students to campus through a series of exciting Orientation events. This email serves as a notification to make sure you know when the major events are occurring, to ensure they don't interrupt your work and so that you are able to encourage all new students to attend. Brunswick All Students Welcome, 23 February 12 - 1:30pm Brunswick Courtyard. Bundoora All Students Welcome, 24 February 12 - 2pm Bundoora West concourse. City All Students Welcome, 25 February 11am - 2:30pm Alumni Courtyard, University Way. RUSU Welcome Bash, 25 February 4pm - 9pm Alumni Courtyard. City Clubs Day, 3 March 11am - 2pm Alumni Courtyard, University Way.";
+my ($matches) = $datetime =~ /(\d{1,2})t?h?(january|jan|february|feb|march|mar|april|apr|may|june|jun|july|jul|august|aug|september|sept|sep|october|oct|november|nov|december|dec)(\d{1,2})(am|pm)?:?(\d{2})?-t?o?(\d{1,2}):?(\d{2})?(am|pm)?/gi;
 
-$text = $text =~ s/\h//g
+for my $item($matches){
+  print $1;
+}
 
-print $text;
-
-# while($text =~ /(\d\d?)(january|jan|february|feb|march|mar|april|apr|may|june|jun|july|jul|august|aug|september|sept?|october|oct|november|nov|december|dec)\.?(\d{0,23}:?\d\d)(am|pm)?-(\d{0,23}:?\d\d?)(am|pm)?/gi){
-#   print pos $text."\";
-# }
-#
-# # my @array = split(//, $text);
-# #
-# # foreach my $item(@array){
-# #   if($item =~ /(\d\d?)\h(january|jan|february|feb|march|mar|april|apr|may|june|jun|july|jul|august|aug|september|sept?|october|oct|november|nov|december|dec)\.?\h(\d{0,23}:?\d\d)(am|pm)?\h?-?\h(\d{0,23}:?\d\d?)(am|pm)?/gi){
-# #     print $item;
-# #   }
-# # }
+# print Dumper %hash;
