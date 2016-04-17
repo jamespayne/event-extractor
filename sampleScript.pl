1938db70d8e263ddec92cd85bbae41e4e5eca1cb
James Payne <s3435992@student.rmit.edu.au>
2016-04-17 23:37:08 +1000

    Committing sample script for testing...

---
+++ sampleScript.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+
+# Scripting Langugae Programming Assignment 1
+# Perl Email Event Extraction Script by James Payne(s3435992)
+# RMIT University 2016
+
+# Sample test script to demonstrate modularisation.
+
+use eventExtractor;
+
+extractEvents();
