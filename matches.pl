1d8f6b3fe22668fab8dcd23f588382e758a91530
James Payne <s3435992@student.rmit.edu.au>
2016-04-17 16:54:33 +1000

    Dont need this file anymore.

--- matches.pl
+++
@@ -1,7 +0,0 @@
-# forward dd/mm
-
-my dateTime =~ /\b(\d{1,2})t?h?\h(january|jan|february|feb|march|mar|april|apr|may|june|jun|july|jul|august|aug|september|sept|sep|october|oct|november|nov|december|dec)/gi;
-
-# reverse mm/yy
-
-my date =~ /\b(january|jan|february|feb|march|mar|april|apr|may|june|jun|july|jul|august|aug|september|sept|sep|october|oct|november|nov|december|dec)\.?,?\h?(\d{1,2})t?h?,/gi;
