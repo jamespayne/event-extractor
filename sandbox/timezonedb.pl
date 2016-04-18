67745bca13028291e6a101586682fe2017e81c70
James Payne <s3435992@student.rmit.edu.au>
2016-04-18 19:00:25 +1000

    Did some more tests with the timezonedb API

--- sandbox/timezonedb.pl
+++ sandbox/timezonedb.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/env perl
-my $timezoneData = `curl -s "http://api.timezonedb.com/?zone=Australia/Melbourne&format=json&key=5UVEM3ZMSZHG"`."\n";
+print curl -s `"http://api.timezonedb.com/?zone=Australia/Melbourne&format=json&key=5UVEM3ZMSZHG"`;
 
-print $timezoneData;
+my @timezoneData = split(/".?"/, `curl -s "http://api.timezonedb.com/?zone=Australia/Melbourne&format=json&key=5UVEM3ZMSZHG"` );
+
+# print $timezoneData;
+
+foreach $item(@timezoneData){
+  print "$item\n";
+}
