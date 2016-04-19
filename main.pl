d74b3d7450653b80d67de91cda344d442fad41da
James Payne <s3435992@student.rmit.edu.au>
2016-04-19 15:02:11 +1000

    Initial commit of main.pl script.
    This file was created to implement modularisation.

---
+++ main.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+# Scripting Langugae Programming Assignment 1
+# Perl Email Event Extraction Script by James Payne(s3435992)
+# RMIT University 2016
+
+# Sample test script to demonstrate modularisation.
+
+use strict;
+use warnings;
+use eventExtractor;
+
+if (!$ARGV[0] || @ARGV > 1){
+  print "\nYou didn't supply an input file or you have supplied too many\n";
+  print "arguments. Please read the documentation!\n\n";
+  exit;
+}
+
+# Check the file name argument has a .json extension.
+
+if ($ARGV[0] =~ /\.json$/i){
+  print "Valid .json file submitted. Thank you.\n";
+} else {
+  print "That doesn't appear to be a valid .json file.\n";
+  exit;
+}
+
+#call the main subroutine in eventExtractor.pm perl module.
+
+eventExtractor::extractEvents();
